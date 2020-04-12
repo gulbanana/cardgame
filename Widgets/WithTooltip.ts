e22b63d96a0740c976eb855ff3dac6966bf0716b
Thomas Castiglione <castiglione@mac.com>
2020-04-12 15:00:12 +0800

    truly client-side tooltips for better performance

--- Widgets/WithTooltip.ts
+++ Widgets/WithTooltip.ts
@@ -1,15 +1,29 @@
 namespace WithTooltip {
+    export function register(content: HTMLElement, tooltip: HTMLElement) {
+        content.onmouseenter = (ev) => reposition(tooltip, ev.clientX, ev.clientY);
+        content.onmousemove = (ev) => reposition(tooltip, ev.clientX, ev.clientY);
+        content.onmouseleave = (ev) => reposition(tooltip, ev.clientX, ev.clientY);
+    }
+
     export function reposition(element: HTMLElement, x: number, y: number) {
         element.classList.add("with-tooltip__tooltip--visible");
 
         element.style.left = x+2 + "px";
 
         if (window.innerHeight - y < 200) {
-            element.querySelector(".magnify").classList.add("magnify--bottom-left");
+            let zoomedChild = element.querySelector(".magnify");
+            if (zoomedChild != null) {
+                zoomedChild.classList.add("magnify--bottom-left");
+            }
+            
             element.style.top = null;
             element.style.bottom = (window.innerHeight - y) + 2 + "px";
         } else {
-            element.querySelector(".magnify").parentElement.classList.remove("magnify--bottom-left");
+            let zoomedChild = element.querySelector(".magnify");
+            if (zoomedChild != null) {
+                zoomedChild.classList.remove("magnify--bottom-left");
+            }
+
             element.style.top = y + 2 + "px";
             element.style.bottom = null;
         }
--- wwwroot/app.js
+++ wwwroot/app.js
@@ -1,16 +1,28 @@
 "use strict";
 var WithTooltip;
 (function (WithTooltip) {
+    function register(content, tooltip) {
+        content.onmouseenter = (ev) => reposition(tooltip, ev.clientX, ev.clientY);
+        content.onmousemove = (ev) => reposition(tooltip, ev.clientX, ev.clientY);
+        content.onmouseleave = (ev) => reposition(tooltip, ev.clientX, ev.clientY);
+    }
+    WithTooltip.register = register;
     function reposition(element, x, y) {
         element.classList.add("with-tooltip__tooltip--visible");
         element.style.left = x + 2 + "px";
         if (window.innerHeight - y < 200) {
-            element.querySelector(".magnify").classList.add("magnify--bottom-left");
+            let zoomedChild = element.querySelector(".magnify");
+            if (zoomedChild != null) {
+                zoomedChild.classList.add("magnify--bottom-left");
+            }
             element.style.top = null;
             element.style.bottom = (window.innerHeight - y) + 2 + "px";
         }
         else {
-            element.querySelector(".magnify").parentElement.classList.remove("magnify--bottom-left");
+            let zoomedChild = element.querySelector(".magnify");
+            if (zoomedChild != null) {
+                zoomedChild.classList.remove("magnify--bottom-left");
+            }
             element.style.top = y + 2 + "px";
             element.style.bottom = null;
         }
