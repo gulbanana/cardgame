8310f086b19f4c01132626cea02e9fb3dcebeb74
Thomas Castiglione <castiglione@mac.com>
2020-04-16 02:24:47 +0800

    implement Moneylender

---
+++ Cards/Dominion/Moneylender.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cardgame.Cards.Dominion
+{
+    public class Moneylender : ActionCardModel
+    {
+        public override string Art => "dom-moneylender";
+        public override int Cost => 4;
+        
+        public override string Text => @"<spans>
+            <run>You may trash a Coppper from your hand for</run>
+            <sym prefix='+' suffix='.'>coin3</sym>
+        </spans>";
+
+        protected override async Task ActAsync(IActionHost host)
+        {
+            if (host.GetHand().Any(card => card is Base.Copper))
+            {
+                if (await host.YesNo("Moneylender", $@"<run>Do you want to trash a Copper?</run>"))
+                {
+                    host.Trash("Copper");
+                    host.AddMoney(3);
+                }
+            }
+        }
+    }
+}
