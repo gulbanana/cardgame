7beb7c4a0e1011a0b29d75ec3759b8ae29e1c22f
Thomas Castiglione <castiglione@mac.com>
2020-04-16 02:15:43 +0800

    implement Laboratory

---
+++ Cards/Dominion/Laboratory.cs
@@ -0,0 +1,22 @@
+namespace Cardgame.Cards.Dominion
+{
+    public class Laboratory : ActionCardModel
+    {
+        public override string Art => "dom-laboratory";
+        public override int Cost => 5;
+        
+        public override string Text => @"
+        <block>
+            <lines>
+                <run>+2 Cards</run>
+                <run>+1 Action</run>
+            </lines>
+        </block>";
+
+        protected override void Act(IActionHost host)
+        {
+            host.DrawCards(2);
+            host.AddActions(1);
+        }
+    }
+}
