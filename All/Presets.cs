f5aa2dc2b48b5ca16e30d3d0e161b288c47edb20
Thomas Castiglione <castiglione@mac.com>
2020-04-18 20:47:06 +0800

    more Intrigue presets

--- All/Presets.cs
+++ All/Presets.cs
@@ -14,7 +14,7 @@ namespace Cardgame.All
                 { "Big Money", new[]{ "Adventurer", "Bureaucrat", "Chancellor", "Chapel", "Feast", "Laboratory", "Market", "Mine", "Moneylender", "ThroneRoom" }},
                 { "Interaction", new[]{ "Bureaucrat", "Chancellor", "CouncilRoom", "Festival", "Library", "Militia", "Moat", "Spy", "Thief", "Village" }},
                 { "Size Distortion", new[]{ "Cellar", "Chapel", "Feast", "Gardens", "Laboratory", "Thief", "Village", "Witch", "Woodcutter", "Workshop" }},
-                { "Village Square", new[]{ "Bureaucrat", "Cellar", "Festival", "Library", "Market", "Remodel", "Smithy", "ThroneRoom", "Village", "Woodcutter" }}
+                { "Village Square", new[]{ "Bureaucrat", "Cellar", "Festival", "Library", "Market", "Remodel", "Smithy", "ThroneRoom", "Village", "Woodcutter" }},
             } },
             { CardSet.Dominion2nd, new Dictionary<string, string[]> {
                 { "First Game", new[]{ "Cellar", "Market", "Merchant", "Militia", "Mine", "Moat", "Remodel", "Smithy", "Village", "Workshop" }},
@@ -22,13 +22,17 @@ namespace Cardgame.All
                 { "Deck Top", new[]{ "Artisan", "Bureaucrat", "CouncilRoom", "Festival", "Harbinger", "Laboratory", "Moneylender", "Sentry", "Vassal", "Village" }},
                 { "Sleight of Hand", new[]{ "Cellar", "CouncilRoom", "Festival", "Gardens", "Library", "Harbinger", "Militia", "Poacher", "Smithy", "ThroneRoom" }},
                 { "Improvements", new[]{ "Artisan", "Cellar", "Market", "Merchant", "Mine", "Moat", "Moneylender", "Poacher", "Remodel", "Witch" }},
-                { "Silver & Gold", new[]{ "Bandit", "Bureaucrat", "Chapel", "Harbinger", "Laboratory", "Merchant", "Mine", "Moneylender", "ThroneRoom", "Vassal" }}
+                { "Silver & Gold", new[]{ "Bandit", "Bureaucrat", "Chapel", "Harbinger", "Laboratory", "Merchant", "Mine", "Moneylender", "ThroneRoom", "Vassal" }},
             } },
             { CardSet.Intrigue1st, new Dictionary<string, string[]> {
-                { "Victory Dance", new[] { "Bridge", "Duke", "GreatHall", "Harem", "Ironworks", "Masquerade", "Nobles", "Pawn", "Scout", "Upgrade" } }
+                { "Victory Dance", new[] { "Bridge", "Duke", "GreatHall", "Harem", "Ironworks", "Masquerade", "Nobles", "Pawn", "Scout", "Upgrade" } },
+                { "Secret Schemes", new[] { "Conspirator", "Harem", "Ironworks", "Pawn", "Saboteur", "ShantyTown", "Steward", "Swindler", "TradingPost", "Tribute" } },
+                { "Best Wishes", new[] { "Coppersmith", "Courtyard", "Masquerade", "Scout", "ShantyTown", "Steward", "Torturer", "TradingPost", "Upgrade", "WishingWell" } }
             } },
             { CardSet.Intrigue2nd, new Dictionary<string, string[]> {
-                { "Victory Dance", new[] { "Baron", "Courtier", "Duke", "Harem", "Ironworks", "Masquerade", "Mill", "Nobles", "Patrol", "Replace" } }
+                { "Victory Dance", new[] { "Baron", "Courtier", "Duke", "Harem", "Ironworks", "Masquerade", "Mill", "Nobles", "Patrol", "Replace" } },
+                { "The Plot Thickens", new[] { "Conspirator", "Ironworks", "Lurker", "Pawn", "MiningVillage", "SecretPassage", "Steward", "Swindler", "Torturer", "TradingPost" } },
+                { "Best Wishes", new[] { "Baron", "Conspirator", "Courtyard", "Diplomat", "Duke", "SecretPassage", "ShantyTown", "Torturer", "Upgrade", "WishingWell" } },
             } }
         };
 
