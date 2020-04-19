c88bbea36950b69f53267ef6b5f415582aa5a8e7
Thomas Castiglione <castiglione@mac.com>
2020-04-19 18:06:37 +0800

    implement Mining Village

---
+++ Cards/Intrigue/MiningVillage.cs
@@ -0,0 +1,28 @@
+using System.Threading.Tasks;
+using Cardgame.API;
+
+namespace Cardgame.Cards.Dominion
+{
+    public class MiningVillage : ActionCardBase
+    {
+        public override string Art => "int-mining-village";
+        public override int Cost => 4;
+        
+        public override string Text => @"<lines>
+            <bold>+1 Card</bold>
+            <bold>+2 Actions</bold>
+        </lines>";
+
+        protected override async Task ActAsync(IActionHost host)
+        {
+            host.DrawCards(1);
+            host.AddActions(2);
+
+            if (await host.YesNo("Mining Village", "<run>Trash</run><card>MiningVillage</card><run>for</run><sym prefix='+' suffix='?'>coin2</sym>"))
+            {
+                host.Trash("MiningVillage", Zone.InPlay);
+                host.AddMoney(2);
+            }
+        }
+    }
+}
