992a48f6d9cdeab063146284ab5741ba2e9e260b
Thomas Castiglione <castiglione@mac.com>
2020-04-24 06:57:24 +0800

    implement Fishing Village

---
+++ Cards/Seaside/FishingVillage.cs
@@ -0,0 +1,34 @@
+using Cardgame.API;
+
+namespace Cardgame.Cards.Seaside
+{
+    public class FishingVillage : DurationCardBase
+    {
+        public override int Cost => 3;
+
+        public override string Text => @"<paras>
+            <lines>
+                <bold>+2 Actions</bold>
+                <bold><sym prefix='+'>coin1</sym></bold>
+            </lines>
+            <spans>
+                <run>At the start of your next turn:</run>
+                <bold>+1 Action</bold>
+                <run>and</run>
+                <bold><sym prefix='+' suffix='.'>coin1</sym></bold>
+            </spans>
+        </paras>";
+
+        protected override void Act(IActionHost host)
+        {
+            host.AddActions(2);
+            host.AddCoins(1);
+        }
+
+        protected override void OnBeginTurn(IActionHost host)
+        {
+            host.AddActions(1);
+            host.AddCoins(1);
+        }
+    }
+}
