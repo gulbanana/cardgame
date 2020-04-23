99cdf0597bb035263fa15d2919d5e1cf9b922cef
Thomas Castiglione <castiglione@mac.com>
2020-04-24 07:22:38 +0800

    implement Cutpurse

---
+++ Cards/Seaside/Cutpurse.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Cardgame.API;
+
+namespace Cardgame.Cards.Seaside
+{
+    public class Cutpurse : AttackCardBase
+    {
+        public override int Cost => 4;
+
+        public override string Text => @"<paras>
+            <bold><sym prefix='+'>coin2</sym></bold>
+            <run>Each other player discards a Copper (or reveals a hand with no Copper).</run>
+        </paras>";
+
+        protected override async Task ActAsync(IActionHost host)
+        {
+            host.AddCoins(2);
+
+            await host.Attack(target =>
+            {
+                var hand = target.Examine(Zone.Hand);
+                if (hand.Any(card => card.Name == "Copper"))
+                {
+                    target.Discard("Copper");
+                }
+                else
+                {
+                    target.Reveal(Zone.Hand);
+                }
+            });
+        }
+    }
+}
