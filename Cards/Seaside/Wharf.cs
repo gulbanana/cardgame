8276cd0fa7577bc311251958527b45c2dd384075
Thomas Castiglione <castiglione@mac.com>
2020-04-24 04:35:19 +0800

    implement Wharf

---
+++ Cards/Seaside/Wharf.cs
@@ -0,0 +1,30 @@
+using Cardgame.API;
+
+namespace Cardgame.Cards.Seaside
+{
+    public class Wharf : DurationCardBase
+    {
+        public override int Cost => 5;
+
+        public override string Text => @"<lines>
+            <run>Now and at the start of your next turn:</run>
+            <spans>
+                <bold>+2 Cards</bold>
+                <run>and</run>
+                <bold>+1 Buy.</bold>            
+            </spans>
+        </lines>";
+
+        protected override void Act(IActionHost host)
+        {
+            host.DrawCards(2);
+            host.AddBuys(1);
+        }
+
+        protected override void NextTurn(IActionHost host)
+        {
+            host.DrawCards(2);
+            host.AddBuys(1);
+        }
+    }
+}
