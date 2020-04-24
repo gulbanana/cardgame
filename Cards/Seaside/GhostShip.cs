c830b3422fff3d405a098594f57c3d488832f509
Thomas Castiglione <castiglione@mac.com>
2020-04-25 06:28:55 +0800

    implement Ghost Ship

---
+++ Cards/Seaside/GhostShip.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using Cardgame.API;
+
+namespace Cardgame.Cards.Seaside
+{
+    public class GhostShip : AttackCardBase
+    {
+        public override int Cost => 5;
+
+        public override string Text => @"<paras>
+            <bold>+2 Cards</bold>
+            <run>Each other player with 4 or more cards in hand puts cards from their hand onto their deck until they have 3 cards in hand.</run>
+        </paras>";
+
+        protected override async Task ActAsync(IActionHost host)
+        {
+            host.DrawCards(2);
+            await host.Attack(target => target.Count(Zone.Hand) >= 4, async target =>
+            {
+                while (target.Count(Zone.Hand) > 3) {
+                    var put = await target.SelectCard("Choose a card to put onto your deck");
+                    target.PutOnDeck(put);
+                }
+            });
+        }
+    }
+}
