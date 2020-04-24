9d4e323d4696c7248c20eb516b89cb9ec9bba301
Thomas Castiglione <castiglione@mac.com>
2020-04-25 06:37:30 +0800

    implement Merchant Ship

---
+++ Cards/Seaside/MerchantShip.cs
@@ -0,0 +1,24 @@
+using Cardgame.API;
+
+namespace Cardgame.Cards.Seaside
+{
+    public class MerchantShip : DurationCardBase
+    {
+        public override int Cost => 5;
+
+        public override string Text => @"<spans>
+            <run>Now and at the start of your next turn:</run>
+            <sym prefix='+' suffix='.'>coin2</sym>
+        </spans>";
+
+        protected override void Act(IActionHost host)
+        {
+            host.AddCoins(2);
+        }
+
+        protected override void OnBeginTurn(IActionHost host)
+        {
+            host.AddCoins(2);
+        }
+    }
+}
