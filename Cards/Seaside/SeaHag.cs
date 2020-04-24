3d8579f3dbbda138cfc8aab4066cf6f19184615e
Thomas Castiglione <castiglione@mac.com>
2020-04-25 06:05:38 +0800

    implement Sea Hag

---
+++ Cards/Seaside/SeaHag.cs
@@ -0,0 +1,21 @@
+using System.Threading.Tasks;
+using Cardgame.API;
+
+namespace Cardgame.Cards.Seaside
+{
+    public class SeaHag : AttackCardBase
+    {
+        public override int Cost => 4;
+
+        public override string Text => "Each other player discards the top card of their deck, then gains a Curse onto their deck.";
+
+        protected override async Task ActAsync(IActionHost host)
+        {
+            await host.Attack(target =>
+            {
+                target.Discard(Zone.DeckTop(1));
+                target.Gain("Curse", Zone.Deck);
+            });
+        }
+    }
+}
