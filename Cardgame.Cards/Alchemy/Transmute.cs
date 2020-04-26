449fa40a8e806a1e46dbf247b1232ca319fd7c79
Thomas Castiglione <castiglione@mac.com>
2020-04-26 12:47:48 +0800

    implement Transmute and potion-only cost displays

---
+++ Cardgame.Cards/Alchemy/Transmute.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Cardgame.API;
+
+namespace Cardgame.Cards.Alchemy
+{
+    public class Transmute : ActionCardBase
+    {
+        public override Cost Cost => new Cost(0, true);
+
+        public override string Text => @"<paras>
+            <lines>
+                <run>Trash a card from your hand.</run>
+                <run>If it is an...</run>
+            </lines>
+            <lines>
+                <run>Action card, gain a Duchy</run>
+                <run>Treasure card, gain a Transmute</run>
+                <run>Victory card, gain a Gold</run>
+            </lines>
+        </paras>";
+
+        protected override async Task ActAsync(IActionHost host)
+        {
+            var trashed = await host.SelectCard("Choose a card to trash.");
+            if (trashed != null)
+            {
+                if (trashed.Types.Contains(CardType.Action)) host.Gain("Duchy");
+                if (trashed.Types.Contains(CardType.Treasure)) host.Gain("Transmute");
+                if (trashed.Types.Contains(CardType.Victory)) host.Gain("Gold");
+            }
+        }
+    }
+}
