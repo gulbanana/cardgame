29af866e874c8c65ab92797659789b215b7d34ed
Thomas Castiglione <castiglione@mac.com>
2020-05-04 14:13:17 +0800

    in Chrome, MutationObserver triggers too early to implement scrolling (scrollHeight may not yet be updated?) maybe this should be ResizeObserver but there are no typescript defs yet. fix it with a microtask

--- Cardgame.UI/Widgets/VerticalLog.ts
+++ Cardgame.UI/Widgets/VerticalLog.ts
@@ -2,7 +2,9 @@ namespace VerticalLog {
     export function observeAdditions(container: HTMLElement) {
         let config = { childList: true, subtree: true };
         let observer = new MutationObserver((muts, o) => {
-            container.scrollIntoView(false);
+            setTimeout(() => {
+                container.scrollIntoView(false);
+            }, 0);   
         });
         observer.observe(container, config);
     }
--- Cardgame.UI/wwwroot/app.js
+++ Cardgame.UI/wwwroot/app.js
@@ -37,7 +37,9 @@ var VerticalLog;
     function observeAdditions(container) {
         let config = { childList: true, subtree: true };
         let observer = new MutationObserver((muts, o) => {
-            container.scrollIntoView(false);
+            setTimeout(() => {
+                container.scrollIntoView(false);
+            }, 0);
         });
         observer.observe(container, config);
     }
